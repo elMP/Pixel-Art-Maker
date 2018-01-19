9fb04b89dfe2042e04c9ac6f6c3b648df495b4b1
kneazle <kneazle@yandex.ru>
2018-01-19 16:07:11 +0300

    Added makeGrid function - create a grid the specified size

---
+++ makeGrid.js
@@ -0,0 +1,23 @@
+function makeGrid(height, width) {
+    //delete prevous table
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    //create new table
+    for (let i = 0; i < height; ++i) {
+        let newTr = document.createElement('tr');
+        grid.appendChild(newTr);
+        for (let j = 0; j < width; ++j) {
+            let newTd = document.createElement('td');
+            newTr.appendChild(newTd);
+        }
+    }
+}
+
+submitButton.onclick = function() {
+    
+    let h = document.getElementById('height').value;
+    let w = document.getElementById('width').value;
+    makeGrid(h, w);
+}
